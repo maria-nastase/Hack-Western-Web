ea0fbd8e24cf810f0a7bb58efef8da49100cc4f7
Andy Liu <aliu3160@gmail.com>
2024-11-30 15:59:18 -0500

    cleanup

--- hackwesternweb/src/app/api/alert/route.ts
+++ hackwesternweb/src/app/api/alert/route.ts
@@ -3,23 +3,17 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
+const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
 export async function GET(_req) {
     return NextResponse.json({ test: "hello"})
 }
 
-
-
-const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-
-
-
 export async function POST(req) {
-    const test = req.body.hello
     const { fname, lname, number, severity, latitude, longitude } =  req.json();
 
     const locationLink = encodeURI('https://www.google.com/maps/place/43.006841,-81.2769333')
     
-
     client.messages
     .create({
         body: `Hello, we have detected that ${fname} ${lname} has had a ${severity} fall. Please check in with them at the following location: ${locationLink}.`,
