cffa3fe4cf39de682226c42413010048bf3ee4fb
Andy Liu <aliu3160@gmail.com>
2024-11-30 11:54:44 -0500

    integrate twilio

--- hackwesternweb/src/app/api/alert/route.ts
+++ hackwesternweb/src/app/api/alert/route.ts
@@ -1,9 +1,28 @@
 import { NextResponse } from "next/server";
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 export async function GET(_req) {
     return NextResponse.json({ test: "hello"})
 }
 
-export async function POST(_req) {
-    return NextResponse.json({ test: "posted"})
+
+
+const client = require('twilio')(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+
+
+
+export async function POST(req) {
+    const test = req.body.hello
+
+    client.messages
+    .create({
+        body: 'test message',
+        from: '+17754587628',
+        to: '+12262800252'
+    })
+    .then(message => console.log(message.sid));
+
+    return NextResponse.json({ok: true})
 }
