fcdbdd59c6bf88f7bb53431386d6d87a5b8cd6fc
Yang Li <86985694+Yang51st@users.noreply.github.com>
2024-12-01 08:22:02 -0500

    Add files via upload

---
+++ Upright HackwesternTests/Upright_HackwesternTests.swift
@@ -0,0 +1,17 @@
+//
+//  Upright_HackwesternTests.swift
+//  Upright HackwesternTests
+//
+//  Created by Ali Mohammed-Ali on 2024-11-30.
+//
+
+import Testing
+@testable import Upright_Hackwestern
+
+struct Upright_HackwesternTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
